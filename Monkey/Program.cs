98dfb6fb10f6262d232db7d9bc5a8fab14eebab4
РАФАЭЛЬ РЕЗЯПОВ <rezograf@gmail.com>
2020-03-24 14:27:09 +0300

    Создал Clone Monkey в каталоге C:\Users\rezog\source\repos\Monkey\.git Создал проект Monkey в этом же каталоге

---
+++ Monkey/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Monkey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Monkey!");
+        }
+    }
+}
